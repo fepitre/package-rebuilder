44a80e19161ab819e36425b25fd37b0a76185835
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2021-04-02 17:44:04 +0200

    Fix init_feed with latest dist syntax

--- init_feed.py
+++ init_feed.py
@@ -3,12 +3,12 @@
 from app.tasks.rebuilder import get
 
 print("Trigger qubes-r4.1-vm-bullseye...")
-get.delay("qubes-4.1-vm-bullseye", "all")
-get.delay("qubes-4.1-vm-bullseye", "x86_64")
+get.delay("qubes-4.1-vm-bullseye.all")
+get.delay("qubes-4.1-vm-bullseye.x86_64")
 
 print("Trigger bullseye...")
 # "x86_64" is aliased to "amd64"
-get.delay("bullseye+essential+required+build-essential+build-essential-depends", "all")
-get.delay("bullseye+essential+required+build-essential+build-essential-depends", "x86_64")
+get.delay("bullseye+essential+required+build-essential+build-essential-depends.all")
+get.delay("bullseye+essential+required+build-essential+build-essential-depends.x86_64")
 
 print("Done!")
