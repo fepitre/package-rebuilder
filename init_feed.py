7115ac8ad0d966b1ef7be87676bcaaf805bb4ac7
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2021-04-03 10:10:26 +0200

    init_feed: don't alias arch

--- init_feed.py
+++ init_feed.py
@@ -4,11 +4,10 @@ from app.tasks.rebuilder import get
 
 print("Trigger qubes-r4.1-vm-bullseye...")
 get.delay("qubes-4.1-vm-bullseye.all")
-get.delay("qubes-4.1-vm-bullseye.x86_64")
+get.delay("qubes-4.1-vm-bullseye.amd64")
 
 print("Trigger bullseye...")
-# "x86_64" is aliased to "amd64"
 get.delay("bullseye+essential+required+build-essential+build-essential-depends.all")
-get.delay("bullseye+essential+required+build-essential+build-essential-depends.x86_64")
+get.delay("bullseye+essential+required+build-essential+build-essential-depends.amd64")
 
 print("Done!")
