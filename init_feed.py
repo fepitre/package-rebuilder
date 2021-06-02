ada845bdf0ad2e969d5e73e12b1043d5112fa773
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2021-06-02 09:59:57 +0200

    rebuilder.conf: only unstable

--- init_feed.py
+++ init_feed.py
@@ -6,9 +6,9 @@ print("Trigger qubes-r4.1-vm-bullseye...")
 get.delay("qubes-4.1-vm-bullseye.all")
 get.delay("qubes-4.1-vm-bullseye.amd64")
 
-print("Trigger bullseye...")
-get.delay("bullseye+essential+required+build-essential.all")
-get.delay("bullseye+essential+required+build-essential.amd64")
+#print("Trigger bullseye...")
+#get.delay("bullseye+essential+required+build-essential.all")
+#get.delay("bullseye+essential+required+build-essential.amd64")
 
 print("Trigger unstable...")
 get.delay("unstable+essential+required+build-essential.all")
